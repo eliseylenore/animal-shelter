79d6b842124df5966f5570218c96a9826e3116ab
Elise St Hilaire and grinil khanna <elise@>
2017-01-10 13:13:56 -0800

    Add dark background option add black text to the column divs when in dark background

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,12 @@ $(function(){
   $("p.toggle-dark").click(function(){
     $("body").removeClass();
     $("body").addClass("dark-background");
+  });
+});
+
+$(function(){
+  $("p.toggle-light").click(function(){
+    $("body").removeClass();
+    $("body").addClass("light-background");
+  });
 });
